4c6bfd6feaa8764cc342fcd974703130fbaacf36
Steve-Don <92700131+Steve-Don@users.noreply.github.com>
2022-08-07 18:45:25 +0930

    try

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
+//initialize mongoose
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
